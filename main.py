ecd4635ff59bcb94e4d35aa2eb3a984dba28cdb3
katiaacc94 <katia.ass@yandex.ru>
2021-09-02 18:08:30 +0300

    работа с разметкой

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+from kivy.lang import Builder
+
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+
+KV = '''
+BoxLayout:
+    padding: "10dp"
+    size_hint_y: None
+    height: "1000"
+    
+    MDTextField:
+        id: text_field_error
+        multiline: True
+'''
 
 
-class MainApp(MDApp):
+class Test(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen = Builder.load_string(KV)
+
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        self.screen.ids.text_field_error.bind(
+            on_text_validate=self.set_error_message,
+            on_focus=self.set_error_message,
+        )
+        return self.screen
+
+    def set_error_message(self, instance_textfield):
+        self.screen.ids.text_field_error.error = True
 
 
-MainApp().run()
+Test().run()
