c437794e3631c831a7d1d74e41f7e08476e1e7f1
vasiliy <vaszol@mail.ru>
2021-09-03 02:55:31 +0300

    Добавляем цвет окна и компоненты (box+label+button)

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+Window.size = (300, 100)
+Window.clearcolor = (0 / 255, 77 / 255, 64 / 255, 1)
+Window.title = "Приложение"
 
 
 class MainApp(App):
     def build(self):
-        return Label(text="Hello, World")
+        box = BoxLayout()
+        btn = Button(text='Это кнопка!')
+        label = Label(text='Это текст')
+        box.add_widget(label)
+        box.add_widget(btn)
+
+        return box
 
 
 MainApp().run()
