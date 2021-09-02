9f9fc82d67976ac3c2550c251a44d7d89305e838
vasiliy <vaszol@mail.ru>
2021-09-03 02:35:20 +0300

    Пример работы с окном, вывод "Hello, World"

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
-from kivy.lang import Builder
-
-from kivymd.app import MDApp
-
-KV = '''
-BoxLayout:
-    padding: "10dp"
-    size_hint_y: None
-    height: "1000"
-    
-    MDTextField:
-        id: text_field_error
-        multiline: True
-'''
+from kivy.app import App
+from kivy.uix.label import Label
 
 
-class Test(MDApp):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.screen = Builder.load_string(KV)
-
+class MainApp(App):
     def build(self):
-        self.screen.ids.text_field_error.bind(
-            on_text_validate=self.set_error_message,
-            on_focus=self.set_error_message,
-        )
-        return self.screen
-
-    def set_error_message(self, instance_textfield):
-        self.screen.ids.text_field_error.error = True
+        return Label(text="Hello, World")
 
 
-Test().run()
+MainApp().run()
