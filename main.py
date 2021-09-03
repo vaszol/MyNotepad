5a3368a6c308d5e8f119db3d8924b353f1c15cdb
vasiliy <vaszol@mail.ru>
2021-09-03 03:02:37 +0300

    Случайный цвет текста по нажатию на кнопку

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.label import Label
@@ -10,11 +12,20 @@ Window.title = "Приложение"
 
 
 class MainApp(App):
+
+    def __init__(self):
+        super().__init__()
+        self.label = Label(text='Это текст')
+
+    def btn_pressed(self, *args):
+        print("Случайный цвет!")
+        self.label.color = (randint(0, 255) / 255, randint(0, 255) / 255, randint(0, 255) / 255, 1)
+
     def build(self):
         box = BoxLayout()
         btn = Button(text='Это кнопка!')
-        label = Label(text='Это текст')
-        box.add_widget(label)
+        btn.bind(on_press=self.btn_pressed)
+        box.add_widget(self.label)
         box.add_widget(btn)
 
         return box
